dcbd71c3b41f70e7044b8b44d6aa856b4f1bfaf8
Mahmoud Samir <ms5698450@gmail.com>
2024-11-18 20:11:27 +0200

    install nuqs

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -0,0 +1,9 @@
+const workspaceIdPage = () => {
+    return ( 
+        <div>
+        workspace Id
+        </div>
+    );
+}
+
+export default workspaceIdPage;
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,15 +2,36 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
-import { ID } from "node-appwrite";
+import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import { MemberRole } from "@/features/members/types";
+import { generateInviteCode } from "@/lib/utils";
 
 const app = new Hono()
 	.get("/", sessionMiddleware, async (c) => {
+		const user = c.get("user") 
 		const databases = c.get("databases")
+
+		const members = await databases.listDocuments( 
+			DATABASE_ID, 
+			MEMBERS_ID, 
+			[Query.equal("userId", user.$id)] 
+		);
+
+		if (members.total === 0) { 
+			return c.json({ data: { documents: [], total: 0 } }); 
+		}
+		
+
+		const workspaceIds = members.documents.map((member) => member.workspaceId); 
+		
 		const workspaces = await databases.listDocuments(
 			DATABASE_ID,
 			WORKSPACES_ID,
+			[
+				Query.orderDesc("$createdAt"), 
+				Query.contains("$id", workspaceIds) 
+			], 	
 		)
 		return c.json ({data: workspaces})
 	})
@@ -30,12 +51,12 @@ const app = new Hono()
 			const file = await storage.createFile(
 				IMAGES_BUCKET_ID,
 				ID.unique(),
-				image
+				image,
 			);
 
 			const arrayBuffer = await storage.getFilePreview(
 				IMAGES_BUCKET_ID,
-				file.$id
+				file.$id,
 			);
 			uploadedImageUrl = `data:image/png;base64,${Buffer.from(
 				arrayBuffer
@@ -50,11 +71,26 @@ const app = new Hono()
 				name,
 				userId: user.$id,
 				imageUrl: uploadedImageUrl,
-			}
+				inviteCode : generateInviteCode(6),
+			},
+		);
+
+
+		await databases.createDocument(
+			DATABASE_ID,
+			MEMBERS_ID,
+			ID.unique(),
+			{
+				userId: user.$id,
+				workspaceId: workspace.$id,
+				role: MemberRole.ADMIN,
+			},
 		);
 
 		return c.json({ data: workspace });
 	}
 );
+
+
 
 export default app;
